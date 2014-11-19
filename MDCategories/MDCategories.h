291b3e01d26d2ca50d85de30d9e70db89d9c4183
DeWittmm <dewittmm@gmail.com>
2014-11-19 09:13:24 -0800

    ADD: Unified header, and restructured into folder groups

---
+++ MDCategories/MDCategories.h
@@ -0,0 +1,31 @@
+//
+//  MDCategories.h
+//  MDCategories
+//
+//  Created by Michael DeWitt on 11/19/14.
+//  Copyright (c) 2014 Michael DeWitt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef MDCategories_MDCategories_h
+#define MDCategories_MDCategories_h
+
+//Debugging
+#import "NSIndexPath+PrintableIndexPaht.h"
+
+//NSString
+#import "NSString+AddOns.h"
+
+//NSDate
+#import "NSDate+DateComponents.h"
+#import "NSDate+DateInRange.h"
+
+//NSMutableArray
+#import "NSMutableArray+Removal.h"
+
+//UI Classes
+#import "UIColor+Perception.h"
+#import "UIView+Frame.h"
+
+#endif
